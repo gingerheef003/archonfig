8b88a90494e98e59f8dd7861214c24aba768b374
Ginger Heef <gingerheef003@gmail.com>
2022-07-14 17:58:24 +0530

    mod sxhkd media_controls

---
+++ sxhkd/media_control.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+func=$1
+
+bus=`dbus-send --session\
+	--type=method_call\
+	--print-reply=literal\
+	--dest=org.freedesktop.DBus\
+	/org/freedesktop/DBus\
+	org.freedesktop.DBus.ListNames |
+	tr ' ' '\n' |
+	grep 'org.mpris.MediaPlayer2' |
+	head -n 1`
+
+dbus-send --type=method_call --dest=$bus /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.$func
