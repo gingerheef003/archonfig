0d5b762f6314e031b1872ddcfd48ad41db936f9f
Ginger Heef <gingerheef003@gmail.com>
2022-10-26 17:27:03 +0530

    mod dunst volume_brightness_icons

--- sxhkd/brightness_control.sh
+++ sxhkd/brightness_control.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 func=$1
+icon="/usr/share/icons/Adwaita/32x32/status/display-brightness-symbolic.symbolic.png"
 
 function show_brightness {
 	brightness=$(xbacklight -get | cut -d '.' -f 1)	
-	dunstify -r 548565 -a "Brightness" -h int:value:$brightness "BRIGHTNESS: $brightness%"
+	dunstify -r 548565 -i $icon -a "Brightness" -h int:value:$brightness "$brightness%"
 }
 
 case $func in
 	up)
 		xbacklight -inc 5
-		show_brightness
 		;;
 	down)
 		xbacklight -dec 5
-		show_brightness
 		;;
 esac
+
+show_brightness
--- sxhkd/volume_control.sh
+++ sxhkd/volume_control.sh
@@ -2,10 +2,11 @@
 
 func=$1
 msgTag="volume_notif"
+icon="/usr/share/icons/Adwaita/32x32/status/audio-volume-high-symbolic.symbolic.png"
 
 function show_volume {
 	volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '/Volume:/{print $5}' | tr -dc "[:digit:]")
-	dunstify -r 548566 -a "Volume" -h int:value:$volume "Volume: $volume"
+	dunstify -r 548566 -i $icon -a "Volume" -h int:value:$volume "$volume"
 }
 
 #pactl set-sink-volume @DEFAULT_SINK@ {$func}2%
