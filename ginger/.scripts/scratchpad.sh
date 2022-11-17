d05aab253d4be6d4e0bcd68c1263d605bf3978c1
Ginger Heef <gingerheef003@gmail.com>
2022-11-17 17:04:13 +0530

    mod scratchpad 2.0

--- ginger/.scripts/scratchpad.sh
+++ ginger/.scripts/scratchpad.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-pids=$(xdotool search --class "scratchpad")
+pid=$(xdotool search --class 'scratchpad')
 
-for pid in $pids; do
-	bspc node $pid --flag hidden -f
-done
+if [ -z  $pid ]; then
+	alacritty --class 'scratchpad'
+else
+	if [ ! -f /tmp/scratchpad ]; then
+		touch /tmp/scratchpad && bspc node $pid --flag hidden=off -f
+	elif [ -f /tmp/scratchpad ]; then
+		rm /tmp/scratchpad && bspc node $pid --flag hidden=on
+	fi
+fi
