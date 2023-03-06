3662531f646a80df5f4e686b1e23768f3db1a296
Ginger Heef <gingerheef003@gmail.com>
2023-03-06 11:35:14 +0530

    mod nvim telescope

--- nvim/lua/lazy-config/init.lua
+++ nvim/lua/lazy-config/init.lua
@@ -42,4 +42,10 @@ require("lazy").setup({
 
 	{ "lukas-reineke/indent-blankline.nvim" },
 
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.1',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	}
+
 })
